e6170246e23cdee213a50797e62a26b325bbdcdc
jonpotxu <jonpotxu@gmail.com>
2025-10-17 10:31:52 +0200

    Delete fix.ps1

--- fix.ps1
+++
@@ -1,8 +0,0 @@
-# supabaseClient
-Get-ChildItem -Recurse -File .\pages\landing\ -Include *.js,*.jsx,*.ts,*.tsx |
-  ForEach-Object {
-    (Get-Content $_.FullName) `
-      -replace '\.\./\.\./lib/supabaseClient', '../../../lib/supabaseClient' `
-      -replace '\.\./\.\./utils/', '../../../utils/' |
-    Set-Content $_.FullName
-  }
